7fb66cb4635a4f6d581ace8bae56d546704ced2a
amitkulkarnis@gmail.com <amitkulkarnis@gmail.com@39b6c83e-47ab-4faa-8b8d-e1a33096573b>
2012-02-15 15:18:48 +0000

    update of different clocks tx and rx

--- trunk/twoFF4phase/sync.v
+++ trunk/twoFF4phase/sync.v
@@ -2,7 +2,7 @@ module sync_multi(/*AUTOARG*/
    // Outputs
    out_data, f, d,
    // Inputs
-   in_data, v, clk1, clk2, reset
+   in_data, v, clk_tx, clk_rx, reset
    );
 
 `include "def.v"
@@ -14,7 +14,7 @@ module sync_multi(/*AUTOARG*/
    output [DATA_MSB:0] out_data;
    output 	       f, d;
    input [DATA_MSB:0]  in_data;
-   input 	       v, clk1, clk2, reset;
+   input 	       v, clk_tx, clk_rx, reset;
 
    /*AUTOREG*/
    // Beginning of automatic regs (for this module's undeclared outputs)
@@ -25,8 +25,8 @@ module sync_multi(/*AUTOARG*/
    wire [DATA_MSB:0]   data;   
    wire ack1, req1, f1, d1;
  
-   transmitter tx1(.output_tx(data), .req(req1), .f(f1), .clk(clk1), .v(v), .reset(reset), .ack(ack1), .input_tx(in_data));
-   receiver rx1(.output_rx(out_data), .ack(ack1), .d(d1), .clk(clk2), .reset(reset), .req(req1), .input_rx(data));
+   transmitter tx1(.output_tx(data), .req(req1), .f(f1), .clk(clk_tx), .v(v), .reset(reset), .ack(ack1), .input_tx(in_data));
+   receiver rx1(.output_rx(out_data), .ack(ack1), .d(d1), .clk(clk_rx), .reset(reset), .req(req1), .input_rx(data));
         always @* begin
                 f =f1;
                 d =d1;
