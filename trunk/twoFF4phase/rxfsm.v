9306dd8e99bebe78c7b192c622b251f0d8333caf
amitkulkarnis@gmail.com <amitkulkarnis@gmail.com@39b6c83e-47ab-4faa-8b8d-e1a33096573b>
2012-02-20 11:28:33 +0000

    added reset to sensitivity list

--- trunk/twoFF4phase/rxfsm.v
+++ trunk/twoFF4phase/rxfsm.v
@@ -50,7 +50,7 @@ module receiver(/*AUTOARG*/
       end
    end 
    
-    always@(posedge clk) begin
+    always@(posedge clk or posedge reset) begin
       
        if(reset) begin
 	  current_state <= rst;
