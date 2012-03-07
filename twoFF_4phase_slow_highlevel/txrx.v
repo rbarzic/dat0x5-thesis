becd09c12c8f8881c7cd91569e85ebb455e756a0
amitkulkarnis@gmail.com <amitkulkarnis@gmail.com@39b6c83e-47ab-4faa-8b8d-e1a33096573b>
2012-03-07 08:32:56 +0000

    Genuine

--- twoFF_4phase_slow_highlevel/txrx.v
+++ twoFF_4phase_slow_highlevel/txrx.v
@@ -1,21 +1,28 @@
 `include "timescale.v"
-module tx (/*AUTOARG*/
-	   // Outputs
-	   snt, req, data,
-	   // Inputs
-	   vi, ack, clk, reset, sdata
-	   );
+module tx(/*AUTOARG*/
+   // Outputs
+   data, snt, req,
+   // Inputs
+   sdata, vi, ack, clk, reset
+   );
 `include "def.v"
-   output snt, req;
-   output [DATA_MSB:0] data;
+
+   /* Wishbone interfaces*/
+   /* Inputs to the Synchronizer from wishbone master*/
+   input [DATA_MSB:0]	      sdata;
+   
+   /* Output from the Syncronizer tx to syncronizer rx*/ 
+   output [DATA_MSB:0]	      data;
+    
+   /*Synchronizer interfaces*/
+   output 	       snt, req;
    input 	       vi, ack, clk, reset;
-   input [DATA_MSB:0]  sdata;
+
    
    /*AUTOREG*/
    // Beginning of automatic regs (for this module's undeclared outputs)
-   reg [DATA_MSB:0]    data;
-   wire 	       req;
-   wire 	       snt;
+   reg [DATA_MSB:0]	data;
+   wire			req;
    // End of automatics
    
    reg 		       a2, a1, a2d;
@@ -25,13 +32,14 @@ module tx (/*AUTOARG*/
    
    always @(posedge clk or posedge reset) begin
       if(reset) begin
-	 data <=  {(1+(DATA_MSB)){1'b0}};
+   	 data <=  {(1+(DATA_MSB)){1'b0}};	 
+	 
 	 a1 <= 1'b0;
 	 a2 <= 1'b0;
 	 a2d <= 1'b0;
       end else begin
 	 if(txe) begin
-	    data <= sdata;
+   	    data <= sdata;	 
 	 end
 	 a1 <= ack;
 	 a2 <= a1;
@@ -43,31 +51,39 @@ module tx (/*AUTOARG*/
    
 endmodule // tx
 
-module rx (/*AUTOARG*/
-	   // Outputs
-	   ack, vo, rdata,
-	   // Inputs
-	   req, clk, reset, data
-	   );
+module rx(/*AUTOARG*/
+   // Outputs
+   rdata, ack, vo,
+   // Inputs
+   data, req, clk, reset
+   );
 `include "def.v"
-   
-   output ack, vo;
-   output [DATA_MSB:0] rdata;
-   input 	       req, clk, reset;   
-   input [DATA_MSB:0]  data;
+
+   /*Wishbone interfaces*/   
+   /* Inputs to the Synchronizer from wishbone master*/
+   input [DATA_MSB:0]   data;
+      
+   /* Output from the Syncronizer tx to syncronizer rx*/ 
+   output [DATA_MSB:0]  rdata;
+
+   /*Syncronizer interfaces*/
+   output ack, vo; 
+   input  req, clk, reset;   
+
    
    /*AUTOREG*/
    // Beginning of automatic regs (for this module's undeclared outputs)
-   reg 		       ack;
-   reg [DATA_MSB:0]    rdata;
-   reg 		       vo;
+   reg			ack;
+   reg [DATA_MSB:0]	rdata;
+   reg			vo;
    // End of automatics
    //wire [DATA_MSB:0]	rdata;
    reg 		       r1, r2, r2d;
    
    always @(posedge clk or posedge reset) begin
       if(reset) begin
-	 rdata <=  {(1+(DATA_MSB)){1'b0}};
+   	 rdata <= {(1+(DATA_MSB)){1'b0}};
+	 
 	 r1 <= 1'b0;
 	 r2 <= 1'b0;
 	 r2d <= 1'b0;
@@ -75,7 +91,7 @@ module rx (/*AUTOARG*/
 	 vo <= 1'b0;	 
       end else begin
 	 if(rxe) begin
-	    rdata <= data;	    
+   	    rdata <= data;	
 	 end
 	 r1 <= req;
 	 r2 <= r1;
@@ -85,9 +101,6 @@ module rx (/*AUTOARG*/
       end
    end // rx
    
-   /*always @* begin
-    rdata = rdata1;
-   end*/
    assign rxe = r2 & (~r2d);     
 endmodule // rx
 
