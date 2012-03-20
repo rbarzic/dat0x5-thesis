f1b6a50df59dc74cb2adb21dacf89b88e0a54130
amitkulkarnis@gmail.com <amitkulkarnis@gmail.com@39b6c83e-47ab-4faa-8b8d-e1a33096573b>
2012-03-20 08:49:04 +0000

    clk up/down

--- LDL/wb_LDLinp_gpio/mutex.v
+++ LDL/wb_LDLinp_gpio/mutex.v
@@ -10,7 +10,7 @@ module mutex(/*AUTOARG*/
    output g1, g2;
    input  r1, r2, reset;
    
-   wire   x1, x2;
+   wire   x1, x2, g11, g21;
    
    /*AUTOREG*/
    // Beginning of automatic regs (for this module's undeclared outputs)
@@ -18,28 +18,20 @@ module mutex(/*AUTOARG*/
    reg			g2;
    // End of automatics
    
-   always begin
-        g1 = 1'b0;
-        g2 = 1'b0;
-        wait(r1 || r2);
-        #10;
-        if (r1 && r2) begin
-                #({$random}%100 +10);
-                if ({$random}%2)
-                        g1 = 1'b1;
-                else
-                        g2 = 1'b1;
-        end
-        else if (r1) begin
-                g1 = 1'b1;
-        end
-        else if (r2) begin
-                g2 = 1'b1;
-        end
-        else begin
-                // error
+   always @* begin
+        if(reset) begin
+            g1 = 1'b0;
+            g2 = 1'b0;
+        end else begin
+            g1 = g11;
+            g2 = g21;
         end
    end
    
+   nand nand_1(x1, r1, x2);
+   nand nand_2(x2, r2, x1);
+   nand nand_3(g11, x1, x1, x1);
+   nand nand_4(g21, x2, x2, x2); 
+
 
 endmodule // mutex
