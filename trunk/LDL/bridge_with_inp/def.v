15ea6040d567bea3cc9f40c9dd8e7618c45168b7
amitkulkarnis@gmail.com <amitkulkarnis@gmail.com@39b6c83e-47ab-4faa-8b8d-e1a33096573b>
2012-03-26 11:06:34 +0000

    working model

---
+++ trunk/LDL/bridge_with_inp/def.v
@@ -0,0 +1,13 @@
+parameter  DATA_WIDTH = 1,
+           DATA_MSB = DATA_WIDTH-1, 
+           ADR_WIDTH = 8, 
+           ADR_MSB = ADR_WIDTH - 1, 
+           SEL_WIDTH = 4, 
+           SEL_MSB = SEL_WIDTH - 1, 
+           TAG_WIDTH = 4, 
+           TAG_MSB = TAG_WIDTH - 1,
+           DATA = 8'b1010_1111;
+
+
+
+
---
+++ trunk/LDL/bridge_with_inp/inputport.v
@@ -0,0 +1,55 @@
+`include "timescale.v"
+module inputport(/*AUTOARG*/
+   // Outputs
+   valid, ack_ip, y1,
+   // Inputs
+   clk_inputport, reset, req
+   );
+`include "def.v"
+
+   output valid, ack_ip, y1;
+   input  clk_inputport, reset, req ;
+   event assigning;
+   /*AUTOREG*/
+   // Beginning of automatic regs (for this module's undeclared outputs)
+   wire   y1;
+   reg ack_ip;
+   // End of automatics
+   wire   ask, ask_dash, latched1, valid, ack_l;
+   reg latched;
+
+
+   async async_u0(/*AUTOINST*/
+		  // Outputs
+		  .ack			(ack_l),
+		  .ask			(ask),
+		  // Inputs
+		  .latched		(latched1),
+		  .req			(req),
+		  .reset		(reset));
+
+   cmuller cmuller_U1(/*AUTOINST*/
+		      // Outputs
+		      .c		(ask_dash),
+		      // Inputs
+		      .a		(ask),
+		      .b		(~valid),
+		      .rstn		(reset));
+   
+   mutex mutex_U2(/*AUTOINST*/
+		  // Outputs
+		  .g1			(latched1),
+		  .g2			(y1),
+		  // Inputs
+		  .r1			(ask_dash),
+		  .r2			(clk_inputport),
+                  .reset                (reset));
+   
+   srlatch srlatch_U3(/*AUTOINST*/
+		      // Outputs
+		      .q		(valid),
+		      // Inputs
+		      .s		(latched1),
+		      .r		(y1|reset));
+
+endmodule // inputport
